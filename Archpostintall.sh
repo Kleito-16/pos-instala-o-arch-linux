056ddb44ec3c0aa639d683189dd4ad5376bf5e32
Kleito <kleitodamascena@gmail.com>
2023-04-19 12:39:07 -0300

    revert

--- Archpostintall.sh
+++ Archpostintall.sh
@@ -1,40 +1,33 @@
-!/bin/bash
+#!/bin/bash
 set -e
 
 error_handler() {
-echo "An error occurred on line $1 with exit code $2"
-exit $2
+  echo "An error occurred on line $1 with exit code $2"
+  exit $2
 }
 trap 'error_handler ${LINENO} $?' ERR
 
-Update system
-pacman-key --init
-pacman-key --populate
-pacman -Sy archlinux-keyring
-pacman -Su
-pacman -Syyu
+#pacman
+pacman-key --init && pacman-key --populate && pacman -Sy archlinux-keyring && pacman -Su && pacman -Syyu
 
-##Install packages
-sudo pacman -S firefox yakuake openssh qbittorrent k3b konsole alacritty dolphin ark kate neofetch git wget barrier packagekit-qt5
+# Install packages
+sudo pacman -S firefox yakuake openssh qbittorrent k3b konsole dolphin ark kate neofetch git wget packagekit-qt5 alacritty
 
-##Install yay
-if sudo pacman -S --needed base-devel curl rust && cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd /home; then
-sudo yay -Y --gendb
-yay -Syu --devel
-yay -Y --devel --save
+# Install yay
+if sudo pacman -S --needed  base-devel  base-devel curl rust  && cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd /home; then
+  sudo yay -Y --gendb && yay -Syu --devel && yay -Y --devel --save
 else
-echo "Error installing yay. Skipping to next part."
+  echo "Error installing yay. Skipping to next part."
 fi
 
-##Install asdf
+# Install asdf
 if cd /tmp && git clone https://aur.archlinux.org/asdf-vm.git && cd asdf-vm && makepkg -si && cd /home; then
-source /opt/asdf-vm/asdf.fish
+  source /opt/asdf-vm/asdf.fish
 else
-echo "Error installing asdf. Skipping to next part."
+  echo "Error installing asdf. Skipping to next part."
 fi
 
 ## criar o alias
 echo "alias PACU='sudo pacman -Syyu'" >> ~/.bash_aliases
-
 
 echo "Setup completed successfully."
